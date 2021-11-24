9f5bac7d7820675507b86732226271c0aea58579
Andu15 <estefania_8_3@hotmail.com>
2021-11-24 15:40:04 -0500

    style: add css about home view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,35 @@
 import { Row, Col } from 'antd';
-import logo from '../assets/portadaHome.png';
+import portada from '../assets/portadaHome.png';
 import { Icon } from '@iconify/react';
 
 const Home = () => {
   return (
     <>
-      {/* <Row>
+      <Row align="middle" justify="center">
         <Col xs={12}>
-          <h2>Hola!</h2>
-          <p>Mi nombre es Andrea</p>
-          <p>Soy Frontend Developer</p>
-          <div>
-            <img src="https://img.icons8.com/fluency/48/000000/linkedin.png"/>
-            <img src="https://img.icons8.com/plasticine/50/000000/github.png"/>
-            <img src="https://img.icons8.com/fluency/48/000000/new-post.png"/>
-            <img src="https://img.icons8.com/doodle/48/000000/slack-new.png"/>
-          </div>
+          <section className="container-home">
+            <h2>Hola!</h2>
+            <p className="text-white">Mi nombre es Andrea</p>
+            <p className="text-green">Soy Frontend Developer</p>
+            <div className="container-icons">
+              {/* <a className="redirect" href="https://www.linkedin.com/in/andrea-estefania-blanco-avila-b78036156/"> */}
+                <img src="https://img.icons8.com/fluency/48/000000/linkedin.png"/>
+              {/* </a> */}
+              <img src="https://img.icons8.com/plasticine/50/000000/github.png"/>
+              <img src="https://img.icons8.com/fluency/48/000000/new-post.png"/>
+              <img src="https://img.icons8.com/doodle/48/000000/slack-new.png"/>
+            </div>
+          </section>
         </Col>
         <Col xs={12}>
-          <img src={logo}/>
+          <img className="cover-page" src={portada}/>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <Icon icon="bi:arrow-up-circle-fill" color="#bce276" rotate={2} />
+      <Row align="middle" justify="center">
+        <Col xs={24} >
+          <Icon icon="bi:arrow-up-circle-fill" color="#bce276" height="40" rotate={2}/>
         </Col>
-      </Row> */}
+      </Row>
     </>
   )
 }
