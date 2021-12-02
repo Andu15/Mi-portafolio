dd9d3969b9f5096c51fd2b9c48f2eb085b839b8a
Andu15 <estefania_8_3@hotmail.com>
2021-12-02 17:02:18 -0500

    feat: add serviceworker for pwa behavior

--- public/worker.js
+++ public/worker.js
@@ -3,14 +3,26 @@
 const CACHE_NAME = 'v1_mi_portafolio';
 //include all routes used in app
 const urlsToCache = [
-  '/'
+  './',
+  './index.html',
+  './favicon-16x16.png',
+  './android-chrome-384x384.png',
+  './android-chrome-512x512.png',
+  './android-icon-144x144.png',
+  './android-icon-192x192.png',
+  './android-icon-48x48.png',
+  './AnduLogoTouch.png',
+  './favicon-32x32.png',
+  './favicon-64x64.ico',
+  './favicon-96x96.png',
+  './ms-icon-310x310.png'
 ];
 
 // Install a service worker(se almacena en caché los activos estaticos)
 self.addEventListener('install', (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(cache =>{
       console.log('Opened cache');
       return cache.addAll(urlsToCache)
         .then(()=>self.skipWaiting())
@@ -23,13 +35,15 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   //responde ya sea con el objeto en caché o continuar y busca la url real
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // recupera la petición a la url
+        return fetch(event.request);
+      })
   );
 });
 
